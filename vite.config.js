573213c95099ccf893e3e16878c73855d96b7ede
Paulo Martins <paulo.martins@stellarcontent.com>
2025-06-05 17:53:01 -0300

    Updating docker container to use php 8.2>

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,20 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 3001,
+        hmr: {
+            host: 'localhost',
+            port: 3001,
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+                silenceDeprecations: ["legacy-js-api"],
+            }
+        }
+    }
 });
